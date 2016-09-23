ce410737a84ba0068a4a2999cc06d6dde196d171
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-09-22 20:58:48 -0400

    change files and file names

---
+++ server.js
@@ -0,0 +1,40 @@
+//==========
+//DEPENDENCIES:
+//the following are npm packages needed for server functionality
+//===========
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//===============
+//EXPRESS CONFIG
+//this sets up the basic properties forour express server
+//===============
+var app = express(); //tells node we are creating an "express" server
+var PORT = 3000;//sets initial port
+
+
+//BodyParser makes it easy for our server to interpret data recieved
+//standard code:
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+//===================
+//ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs
+//====================
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+
+
+//==============
+//LISTENER
+//"starts" the server
+//==============
+ app.listen(PORT, function(){
+ 	console.log("app listening on PORT:" + PORT)
+ });
