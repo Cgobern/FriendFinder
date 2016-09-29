ad60bc1e0012e93dbe3576e7464883a11452116a
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-09-28 21:11:40 -0400

    commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,21 @@
 var friendsArray = [
-	{
-  name:"Jean",
-  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores:[
-     "5",
-     "1",
-     "4",
-     "4",
-     "5",
-     "1",
-     "2",
-     "5",
-     "4",
-     "1"
-    ]
-  }
+	// {
+ //  name:"Jean",
+ //  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+ //  scores:[
+ //     "5",
+ //     "1",
+ //     "4",
+ //     "4",
+ //     "5",
+ //     "1",
+ //     "2",
+ //     "5",
+ //     "4",
+ //     "1"
+ //    ]
+ //  }
+ console.log("working");
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
