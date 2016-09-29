359a9a642a5905f8ab3b389d7fb567c1c0e3e266
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-09-28 20:33:28 -0400

    changed route

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -12,9 +12,9 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname, '..', 'public/survey.html'));
 	});
 	
-	app.get('/', function (req, res) {
-		res.sendFile(path.join(__dirname, '..', 'public/home.html'));	
-	});
+	// app.get('/', function (req, res) {
+	// 	res.sendFile(path.join(__dirname, '..', 'public/home.html'));	
+	// });
 
 	//If no match route found default to home
 	app.use('/', function (req, res) {
